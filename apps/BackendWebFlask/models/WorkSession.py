f3a78e3cb90dad27e142891d308cc5f5a924323e
Luis Carlos Rodriguez Medrano <luiscarlos.rodriguezm@udem.edu>
2026-09-03 13:38:49 -0600

    Model correction

--- apps/BackendWebFlask/models/WorkSession.py
+++ apps/BackendWebFlask/models/WorkSession.py
@@ -1,7 +1,7 @@
 from extensions import db
 from sqlalchemy.dialects.postgresql import UUID, TIMESTAMP
 from sqlalchemy.orm import Mapped,mapped_column,relationship
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Boolean
 import uuid
 from datetime import datetime, timezone
 
@@ -10,16 +10,16 @@ class WorkSession(db.Model):
     
     # Atributos
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    started_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(), nullable=False)
-    ended_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(), nullable=False)
+    started_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+    ended_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
     status_id: Mapped[uuid.UUID]  = mapped_column(ForeignKey('cat_session_status.id'), nullable=False)
     user_id: Mapped[uuid.UUID]  = mapped_column(ForeignKey('user.id'), nullable=False)
     operation_id: Mapped[uuid.UUID]  = mapped_column(ForeignKey('operation.id'), nullable=False)
     station_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('capture_station.id'), nullable=False)
     kit_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('kit.id'), nullable=False)
-    atypical_session: Mapped[bool] = mapped_column(bool(), nullable=False)
-    extended_retention: Mapped[bool] = mapped_column(bool(), nullable=False)
-    retention_until: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(), nullable=False)
+    atypical_session: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    extended_retention: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    retention_until: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
     
     # Relaciones
     # WorkSession -> cat_session_status "status"
